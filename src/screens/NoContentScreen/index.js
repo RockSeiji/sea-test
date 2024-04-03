c9842da9ee47d66702c311be77483a2d7ee399aa
RockSeiji <marioseiji@gmail.com>
2024-04-03 15:54:39 -0300

    Initial Commit
    Partial Implementation

---
+++ src/screens/NoContentScreen/index.js
@@ -0,0 +1,11 @@
+import './NoContentScreen.scss'
+
+const NoContentScreen = () => {
+    return (
+        <div className='no-content-message'>
+            <h1>Em Breve</h1>
+        </div>
+    )
+}
+
+export default NoContentScreen
